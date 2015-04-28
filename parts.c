860999175fa4d81e700415da2f540e71d4487400
SaulFabianE <sfestrada10@aol.com>
2015-04-28 00:33:25 -0500

    Parts List 04/28/15
    Signed-off-by: SaulFabianE <sfestrada10@aol.com>

---
+++ parts.c
@@ -0,0 +1,12 @@
+/*
+// *** Parts ***
+// not all parts will be used in the final implementation, some 
+// are needed for the PICKIT3 to work properly with the chip 
+// and others for testing purposes
+//
+// x2) PIC18F24J11
+// x6) 0.1 uF, 20V ceramic capacitor
+// x6) 10 uF, 6.3V or greater, tantalum or ceramic capacitor
+// x2) 10 kΩ resistor
+// x2) 100Ω to 470Ω resistor
+*/
